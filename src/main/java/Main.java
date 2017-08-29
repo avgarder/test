74904349b9a5e8d3efc3353fe8b1c6c00c9fe9cf
Anton Garder <avgarder@yandex-team.ru>
2017-08-29 14:17:45 +0300

    added Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author Anton Garder
+ */
+public class Main {
+    static void run() {
+        Scanner sc = new Scanner(System.in);
+        Integer a = sc.nextInt();
+        Integer b = sc.nextInt();
+        System.out.println(a == b);
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
